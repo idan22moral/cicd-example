a802f4aad129615c3f6527155b329f7724602fa1
Idan Moral <idan22moral@gmail.com>
2021-12-12 15:58:12 +0200

    Added code, tests and gitignore

---
+++ test_example.py
@@ -0,0 +1,25 @@
+import example
+import unittest
+
+
+class MathTest(unittest.TestCase):
+    def test_mul(self):
+        [self.assertEqual(example.mul(a, b), a * b, f'{a} * {b}')
+         for a in range(10 ** 3) for b in range(10 ** 3)]
+
+    def test_div(self):
+        with self.assertRaises(ZeroDivisionError):
+            [self.assertEqual(example.div(a, b), a / b)
+             for a in range(10 ** 3) for b in range(10 ** 3)]
+
+    def test_sum(self):
+        [self.assertEqual(example.sum(a, b), a + b)
+         for a in range(10 ** 3) for b in range(10 ** 3)]
+
+    def test_sub(self):
+        [self.assertEqual(example.sub(a, b), a - b)
+         for a in range(10 ** 3) for b in range(10 ** 3)]
+
+
+if __name__ == "__main__":
+    unittest.main()
